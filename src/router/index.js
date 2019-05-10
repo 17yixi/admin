0b76b6f74a7679afeceec9eb74022fffcd8adafb
Sakurarianyang <964277585@qq.com>
2019-05-11 03:54:07 +0800

    1.3.2

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from "@/views/Login";
+import Admin from "@/views/Admin";
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/admin",
+      name: "Admin",
+      component: Admin
     }
   ]
-})
+});
---
+++ src/views/Admin.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="admin">
+    <el-container>
+    <el-header>
+      <admin-header></admin-header>
+    </el-header>
+    <el-container>
+      <el-aside>
+        <aside-tab></aside-tab>
+      </el-aside>
+      <el-container>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
+  </el-container>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import AsideTab from '@/components/AsideTab.vue';
+import AdminHeader from '@/components/Header.vue'
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    AsideTab,AdminHeader
+  }
+}
+</script>
+
+<style scoped lang="css">
+.admin{
+  width: 100%;
+  height: 100%;
+}
+</style>
